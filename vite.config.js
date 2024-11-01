e945d0a9a44bd2b06564e865a83807cf73dd9690
Abdelrahman_Magdy <abdalrahman.magdy.elawady@gmail.com>
2024-11-02 00:23:31 +0200

    update index.scss path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "src/index.module,scss";`,
+        additionalData: `@import "src/index.module.scss";`,
       },
     },
   },
